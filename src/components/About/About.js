ab6ccbb20114a8f2b50a80d2a273f9d5333504e5
stephanbazbaz <stephanbazbaz@gmail.com>
2021-08-03 18:42:54 +0300

    resume

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import './style.scss';
+import RESUME from '../../assets/Stephan Benchemoul Resume.pdf'
 export default function About() {
     return (
         <div className='about-container'>
-            <div>About</div>
+
+            <div className='about-text'>Full stack web developer form Tel Aviv, Israel.
+            <br />
+Building websites from small landing
+pages to full systems and E-commerce sites.</div>
+            <a href={RESUME} download="Stephan Benchemoul Resume">
+                <button className='cv-button' type="button">DOWNLOAD CV</button>
+            </a>
         </div>
     )
 }
