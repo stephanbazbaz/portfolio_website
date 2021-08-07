65e8e468883e0f48170823d888d3cfcc0f44fcb4
stephanbazbaz <stephanbazbaz@gmail.com>
2021-08-07 19:33:04 +0300

    pics

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './style.scss';
 import './mediaQuery.scss';
+import VACATIONS from '../../assets/Screen Shot 2021-08-07 at 7.08.22 PM.png';
+import SUPERMARKET from '../../assets/Screen Shot 2021-08-07 at 7.09.51 PM.png';
+import HOR from '../../assets/Screen Shot 2021-08-07 at 7.10.20 PM.png';
+
 export default function Projects() {
     return (
         <div className='projects-container'>
@@ -16,7 +20,8 @@ export default function Projects() {
                             <div className='project-text'>{item.description}</div>
                             <div className='project-about'>Info:</div>
                             <div className='project-text'>{item.info}</div>
-                            <iframe title={item.name} src={item.link}></iframe>
+                            {/* <iframe title={item.name} src={item.link}></iframe> */}
+                            <div><img src={item.pic}/></div>
                         </div>
                     )
                 })}
@@ -29,18 +34,21 @@ let projectsArr = [
         name: 'HÖR',
         link: 'https://develop.d2n3m2yjy08u8r.amplifyapp.com/',
         description: 'Building the UI for the DJ live sets App - "HÖR"',
-        info: 'Front end build with React.js, work in progress'
+        info: 'Front end build with React.js, work in progress',
+        pic: HOR
     },
     {
         name: 'SupermarketApp',
         link: 'https://stephansupermarketapp.herokuapp.com/',
         description: 'Final project from my studies at Jhon bryce Academy. Full E-commerce website of a Vegan Supermarket.',
-        info: 'Front end build with React.js, Backend with Node.js and database with D-beaver, site hosted by Heroku.'
+        info: 'Front end build with React.js, Backend with Node.js and database with D-beaver, site hosted by Heroku.',
+        pic: SUPERMARKET
     },
     {
         name: 'VacationsApp',
         link: 'https://stark-basin-64884.herokuapp.com/',
         description: 'Second project from my studies at Jhon bryce Academy emulating a vacation App where you can search & follow vacations.',
-        info: 'Front end build with React.js, Backend with Node.js and database with SQL, site hosted by Heroku. you can login to Admin to upload new vacations using admin username: adminadmin, password: admin1234.'
+        info: 'Front end build with React.js, Backend with Node.js and database with SQL, site hosted by Heroku. you can login to Admin to upload new vacations using admin username: adminadmin, password: admin1234.',
+        pic: VACATIONS
     }
 ]
